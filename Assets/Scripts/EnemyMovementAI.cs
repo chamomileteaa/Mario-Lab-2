c925b7f98f986956b6b19c7355ff53afa3917a2f
Camille Treichler <camilletreichler@icloud.com>
2026-02-24 22:52:37 +1100

    Enemy AI Movement
    Flips when hits wall (Tag) - can change to layer if needed

--- Assets/Scripts/EnemyMovementAI.cs
+++ Assets/Scripts/EnemyMovementAI.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class EnemyMovementAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 1.5f;
+    int direction = -1;
+
+    Rigidbody2D rb;
+    Vector2 velocity;
+
+    
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
+    }
+
+
+    // Turn around when hitting wall
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            direction *= -1;
+            Flip();
+        }
+
+        // Mario contact goomba kills -> 
+        // need to add stomp and fireball different death so mario not invincible 
+        // and ability for goomba to hurt mario
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Die();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
