43e5146a8df433e92ee778c4d1da24fcb3698d0f
SilverString5 <siennapinewillow@gmail.com>
2026-02-26 14:08:24 +1100

    Making progress for One Up Mushroom logic (and setting up power up audio)

---
+++ Assets/Scripts/Powerups/OneUpMushroomController.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class OneUpMushroomController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<MarioController>())
+        {
+            Debug.Log("Mario has been hit!");
+            if (GameData.lives < 3)
+            {
+                GameData.lives++;
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
