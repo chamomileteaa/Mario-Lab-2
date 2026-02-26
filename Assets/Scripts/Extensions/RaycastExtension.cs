160fedb8743bcb59f9e3b14d0ce638c1608662ff
SilverString5 <siennapinewillow@gmail.com>
2026-02-26 12:00:19 +1100

    Making progress with Powerup Movement (added a Raycast Extension)

---
+++ Assets/Scripts/Extensions/RaycastExtension.cs
@@ -0,0 +1,30 @@
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Vector2 = UnityEngine.Vector2;
+
+public static class RaycastExtension
+{
+    private static LayerMask _layerMask = LayerMask.GetMask("Default");
+    public static bool Raycast(this Rigidbody2D rigidbody, Vector2 direction)
+    {
+        if (rigidbody.isKinematic)
+        {
+            return false;
+        }
+
+        float radius = 0.25f;
+        float distance = 0.375f;
+
+        RaycastHit2D hit = Physics2D.CircleCast(rigidbody.position, radius, direction.normalized, distance, _layerMask);
+        return hit.collider != null && hit.rigidbody != rigidbody;
+        
+        
+    }
+
+    public static bool DotTest(this Transform transform, Transform other, Vector2 testDirection)
+    {
+        Vector2 direction = other.position - transform.position;
+        return Vector2.Dot(direction.normalized, testDirection) > 0.25f;
+    }
+}
