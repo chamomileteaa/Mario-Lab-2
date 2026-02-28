c19c9ee9e5acdf6020ce7410bfe87b1ab2dc8b0e
SlipperySalmon999 <noelgalan999@gmail.com>
2026-02-28 13:20:14 +1100

    flag pole itself is done now to wire it up to mario  so it can actually add score to mario

---
+++ Assets/FlagPoleCalculations.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.LowLevelPhysics2D;
+
+public class FlagPoleCalculations : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    //retuern the part ofthe pole that mario touches
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        
+        PoleZone zone = other.GetComponent<PoleZone>();
+
+        int score = zone.GoalScore;
+        
+        Debug.Log("Added score: " + score);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
